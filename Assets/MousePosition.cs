385fe28e3966a066a969ffacd108e890714ad388
Dan <igaryhe@gmail.com>
2020-11-15 02:42:38 +0800

    init

---
+++ Assets/MousePosition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    public Transform connectedTransform;
+    
+    private void Update()
+    {
+        Vector2 mousePositionInScreen = Input.mousePosition;
+		
+        //clamp the mouse position so it's limited to the window bounds
+        mousePositionInScreen.x = Mathf.Clamp(mousePositionInScreen.x,0f,Camera.main.pixelWidth);
+        mousePositionInScreen.y = Mathf.Clamp(mousePositionInScreen.y,0f,Camera.main.pixelHeight);
+
+        //now translate this position to world coordinates
+        var mousePositionInWorld = Camera.main.ScreenToWorldPoint(mousePositionInScreen);
+
+        //move the mouse target to the mouse position
+        transform.position = mousePositionInWorld;
+    }
+    
+    void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position,  connectedTransform.position);
+    }
+}
---
+++ Assets/Neck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class Neck : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody2D.AddForce(Vector2.up * 50);
+        }
+    }
+}
