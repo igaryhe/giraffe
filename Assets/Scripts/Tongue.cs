704316f7a84d74aabc0cca475d7a0942c36b53b3
frogfrogforg <frogfrogforg@gmail.com>
2020-11-16 20:35:10 -0500

    lil code

--- Assets/Scripts/Tongue.cs
+++ Assets/Scripts/Tongue.cs
@@ -8,6 +8,8 @@ public class Tongue : MonoBehaviour
     public GameObject tongueGO;
     public GameObject targetGO;
 
+    public Transform mouth;
+
     public MousePosition mouse;
 
     // Start is called before the first frame update
@@ -23,14 +25,28 @@ public class Tongue : MonoBehaviour
         print("hello!");
 
         targetGO.SetActive(true);
+        tongueGO.SetActive(true);
         // use mouse position to set the transform of the target
+	//
+	
+	Vector3 mouthPos = mouth.position;
+	Vector3 targetPos = targetGO.transform.position;
 
         targetGO.transform.position = mouse.transform.position;
 
-        tongueGO.SetActive(true);
 
         // stretch the tongue so that it reaches from tip of head to
         // the target
+	//
+	// 
+	Vector3 midpoint = (mouth.position + 
+			    targetGO.transform.position) / 2;
+	tongueGO.transform.position = midpoint;
+			   
+	
+	// probably want position = midpoint(mouth, target)
+	// and rotation = angle(mouth, target) or sometthing
+	// and scale = distance(mouth, target)
 
 
       }
