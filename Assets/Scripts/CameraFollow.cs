dcf449afef5c9e84920a3db6f22deb52f23ccf1d
fran <franmorojo@gmail.com>
2020-11-24 22:35:20 -0500

    camera follows and doesn't go outside of background

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,10 +7,15 @@ public class CameraFollow : MonoBehaviour
 
     public Transform target;
 
+    public Transform upperLimit;
+    public Transform lowerLimit;
+
+    private Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -18,7 +23,35 @@ public class CameraFollow : MonoBehaviour
     {
       // follow the target
       Vector3 targetPos = target.transform.position;
+
+      // keep camera at a distance
       targetPos.z = -10;
+
+      // don't let camera go too high or too low
+
+      // this is the camera's orthogrpahic half-size
+      float size = cam.orthographicSize;
+      targetPos.y = Mathf.Clamp(targetPos.y,
+                               upperLimit.position.y + size,
+                               lowerLimit.position.y - size);
+
+
+
+
       transform.position = targetPos;
+    }
+
+    void OnDrawGizmosSelected() {
+//       Gizmos.color = Color.blue;
+// 
+// 
+//       // draw upper limit line
+//       Gizmos.DrawLine(upperLimit.position., );
+// 
+// 
+//       // draw lower limit line
+//       Gizmos.DrawLine( , );
+// 
+// 
     }
 }
